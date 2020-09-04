444d280f6c477decd4995c1cfd0fddd7f300f2a7
Dragonaft <supperator9@gmail.com>
2020-09-04 15:53:38 +0300

    Staff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,31 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule, Routes} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import {NotFoundComponent} from './app.not-found.component';
 import { TodolistComponent } from './todolist/todolist.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { ProfileComponent } from './profile/profile.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 
 const appRoutes: Routes = [
   { path: '', component: LoginFormComponent},
   { path: 'main', component: TodolistComponent},
   { path: 'registration', component: RegistrationFormComponent},
+  { path: 'main/profile', component: ProfileComponent},
   { path: '**', component: NotFoundComponent}
 ];
 
@@ -35,7 +44,8 @@ const appRoutes: Routes = [
     LoginFormComponent,
     RegistrationFormComponent,
     NotFoundComponent,
-    TodolistComponent
+    TodolistComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +61,15 @@ const appRoutes: Routes = [
     MatToolbarModule,
     MatListModule,
     MatCardModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatMenuModule,
+    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
